bb1cecdc2799eb26c86780969ccfe10de90f5118
jaygupta-git <jayvishnugupta@gmail.com>
2023-06-18 20:06:22 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const LocalStorage = require('node-localstorage').LocalStorage;
-var localStorage = new LocalStorage('./scratch');
+var localStorage = new LocalStorage('../scratch/');
+
 const User = require("../models/User");
 const Admin = require("../models/Admin");
 
